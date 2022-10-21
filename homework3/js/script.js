25ec00d1f0e8fd792d9324261955103f29735a4e
bexroz <bexrozsamadov20@gmail.com>
2022-10-21 23:21:33 +0300

    work

--- homework3/js/script.js
+++ homework3/js/script.js
@@ -1,12 +1,279 @@
-function getCardNumber(number){
-    if( number==Number(number) && number.length==16 && number.substring(4,0)=="8600" ){
-        alert(number.slice(0,4)+" **** **** "+number.substr(-4,4))
-    }else if( number==Number(number) && number.length> 16 || number.length< 16  ){
-        alert(`16ta raqamdan kam yoki ko'p`)
-    }else if( !(number.slice(0, 4)=="8600") && number==Number(number)  ){
-        alert('!!! Bu UZCARD emas !!!')
-    } else alert('xato')
-    
-}
 
-getCardNumber("8600199009900990")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let animalName = 'quyon'
+// let arr = []
+// let arr2 = new Array()
+// let arr4 = ''.split('')
+
+// let str = `O'rmonda yashaydigan hayvon "${animalName}"`
+// function some(...n){
+//     return [...n]
+// }
+
+// let str2 = `Courses: 
+//             Frontend, 
+//             Backend, 
+//             Android`
+// let arr3 = some(1, 2, 3, 5, 6, 7)
+
+// let str3 = 'O\nrmon'
+// console.log(arr4)
+
+// let str4 = 'Courses: Bu backslesh \nFrontend, \nBackend, \nAndroid'
+
+// console.log(str3.length)
+// console.log("\u{1F60D}")
+// // some()
+// // Math.random()
+
+// // let obj = {
+// //     name: 'alex',
+// //     fn: function(){
+
+// let courseName = 'Frontend/Open/Web'
+// // let c = courseName.slice(0, 8) 
+// //     }
+// // }
+
+// // obj.fn()
+
+// console.log(courseName.substr(-3, 5))
+
+// console.log(courseName.split('e'))
+// let fruits = ['apple', 'peach', 'strawberry', 'kiwi', 'banana']
+// let num = [1, 2, 11, 4, 8]
+
+// // console.log(courseName.substring(5, 0)) 
+// // console.log(courseName.slice(5, 0))  
+
+// // console.log(courseName.substring(-6, -1)) manfiy argument qabul qilmaydi
+// // console.log(courseName.slice(-6, -1))  chapdan o'ngga qarab copy qiladi
+
+// function arrPop(arr){
+//     //code
+// }
+
+// // console.log(c)
+// arrPop(num)
+// arrPop(num)
+// arrPush(num, 8)
+
+// // console.log(courseName[4])
+// // console.log(courseName.charAt(4))
+// // console.log(courseName.length)
+// function arrPush(arr, item){
+
+// // console.log(courseName.indexOf('n', 9))
+// // console.log(courseName.includes('n', 14))
+// // console.log(courseName.indexOf('Open'))
+// }
+// arrPush(num, 10)
+
+
+// // console.log(num.indexOf(2))
+// // console.log(num.includes(2))
+
+// let num2 = 5
+
+// // console.log(fruits.concat(num))
+// // console.log(fruits.splice(0, 4, ...num))
+// // console.log(fruits.slice(0 , 4))
+// // delete num[1]
+// // console.log(fruits.join().toUpperCase().split(','))
+
+
+// // num.sort(function(a, b){
+// //     return a - b  
+// // })
+// // fruits[100] = 'melon'
+
+// // fruits.push('watermelon')
+// // fruits.pop()
+// // fruits.unshift('melon')
+// // fruits.shift()
+// // fruits.reverse()
+// // console.log(fruits['0'])
+// // console.log(fruits[0])
+// // console.log(fruits.length)
+// // console.log(fruits[89])
+// // console.log(fruits[fruits.length - 1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let get= 'Wellcome';
+
+// // // alert(get.length)
+// // // alert(get[1]) //e
+// // //  alert(get.charAt(1)) //e 
+// // //  alert(get[get.length-3])  //o
+// // // alert(get.toUpperCase()) //WELLCOME
+// // // alert(get.toLowerCase())  //welcome
+// // // alert(get[3].toUpperCase()) //L
+// // // alert(get.indexOf('co')) //4
+// // // alert(get.indexOf('e',3))  //7
+// // // alert(get.includes('el')) //true
+// // // alert(get.includes('ds')) //false
+// // // alert(get.slice(2,5)) //llc
+// // // alert(get.slice(3)) //lcome
+// // // alert(get.slice(-4,-1)) //com
+// // // alert(get.substring(2,5)) //llc
+// // // alert(get.substring(5,2)) //llc
+// // // alert(get.substr(2,5))  // llcom
+// // // alert(get.substr(-5,4))   //lcom  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // function getCardNumber(number){
+// //     if( number==Number(number) && number.length==16 && number.substring(4,0)=="8600" ){
+// //         alert(number.slice(0,4)+" **** **** "+number.substr(-4,4))
+// //     }else if( number==Number(number) && number.length> 16 || number.length< 16  ){
+// //         alert(`16ta raqamdan kam yoki ko'p`)
+// //     }else if( !(number.slice(0, 4)=="8600") && number==Number(number)  ){
+// //         alert('!!! Bu UZCARD emas !!!')
+// //     } else alert('xato')
+    
+// // }
+
+// // getCardNumber("8600199009900990")
