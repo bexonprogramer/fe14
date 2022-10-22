23c8f65eb92fa9065f1054341047e6017a03db33
bexroz <bexrozsamadov20@gmail.com>
2022-10-22 12:01:30 +0300

    arr

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -1,44 +1,28 @@
 //task1
-
 function arrPop(add){
     let arr=["malibu","matiz","damas"]
    arr[arr.length-1]=add
     console.log(arr)
 }
-
 arrPop("dsdsd")
 
-// task2
 
+// task2
 function arrPush(add){
     let arr=["malibu","matiz","damas"]
     arr=arr.concat([add])
     alert(arr)
 
 }
-
 arrPush("spark")
 
-   
 
+//task3   
+let names=["jasur", "maxmud","murod","dilshod","bobur"]
 
+for (let i=0 ; names.length>i ; i++){
+    alert((1+i)+") " + names[i])
 
-
-
-
-
-
-
-
-
-
-
-
-// let names=["jasur", "maxmud","murod","dilshod","bobur"]
-
-// for (let i=0 ; names.length>i ; i++){
-//     alert((1+i)+") " + names[i])
-
-// }
+}
 
 
